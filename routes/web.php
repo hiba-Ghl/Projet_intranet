1584406ba98e1aaa14206c4e4a6b2fa74c8d55a9
hamidmbh <hamidmbh54@gmail.com>
2025-04-07 20:36:26 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ChatController;
 // routes/web.php
 
 Route::resource('annonces', HomeController::class);
